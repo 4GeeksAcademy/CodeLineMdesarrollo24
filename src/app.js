ba34d2d12280fabe771069fcd6c31b9b98517b3f
mdesarrollo24 <mar.1298ch@gmail.com>
2024-10-29 21:12:29 +0000

    prueba

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let ext = ['.com', '.net', '.us', '.io']; 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let p of pronoun) {
+  for (let a of adj) {
+    for (let n of noun) {
+      for (let extension of ext) {
+        console.log(`${p}${a}${n}${extension}`);
+      }
+    }
+  }
+}
